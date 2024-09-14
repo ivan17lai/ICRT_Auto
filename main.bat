8cc9709ee23828b8b4666d55804327444024818b
Yihuan <34911327+ivan17lai@users.noreply.github.com>
2024-09-14 11:58:11 +0800

    fix 404 bug

--- main.bat
+++ main.bat
@@ -7,15 +7,34 @@ set month=%date:~5,2%
 set day=%date:~8,2%
 set datepart=%year%%month%%day%
 
+
 echo 目前日期 %year% %datepart%
 echo 下載連結 https://www.icrt.com.tw//en/ext/rss/LunchBox/%datepart%NK.mp3
 
+
+
+curl -I -s  "https://www.icrt.com.tw//en/ext/rss/LunchBox/%datepart%NK.mp3" | findstr /C:"404 Not Found">nul
+
+if %errorlevel% equ 0 (
+    echo 404 沒有今日的音檔
+    pause
+    exit /b 1
+)
+echo 連結正常
+
+
+
+
 curl -o "D:\ICRT_auto\mp3\latest.mp3" "https://www.icrt.com.tw//en/ext/rss/LunchBox/%datepart%NK.mp3"
+if %ERRORLEVEL% NEQ 0 (
+    echo 下載失敗
+    exit /b 1
+)
 
 set file2=D:\ICRT_auto\head\ICRT開頭音樂-巴哈-賜福萬民.mp3
 set file1=D:\ICRT_auto\mp3\latest.mp3
+
 set output_file=D:\ICRT_auto\ICRT.mp3
 
 ffmpeg -y -i "concat:%file1%|%file2%" -acodec copy "%output_file%"
-
 
